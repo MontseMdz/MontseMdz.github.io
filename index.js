da4266925bf124d7573966b33ebedf4eddf3095a
ceit <63829756+MontseMdz@users.noreply.github.com>
2025-09-26 11:47:11 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,270 @@
+/*Variables Globales*/
+let cantidad = 1;
+let cart = [];
+
+//Carga de productos desde el archivo productos.json
+async function loadProducts()  {
+try{
+    const response = await fetch('productos.json');// 1. Carga el archivo
+    const productos = await response.json(); // 2. Analiza la respuesta como JSON
+    displayProductos(productos);
+    console.log(productos);
+}catch(error){
+    console.error('Error al cargar los productos:', error);
+}
+}
+
+//Funciòn para mostrar los productos en el dom
+function displayProductos(productos) {
+    const productList = document.querySelector('.card');
+    productos.forEach(product => {
+        const article = document.createElement('article');
+        article.classList.add('cards__card');
+        article.innerHTML = `
+            <div class="card__img">
+                <img src="${product.imagen}" alt="${product.nombre}">
+            </div>
+            <div class="card__descripcion">
+                <h4 class="descripcion__categoria">${product.categoria}</h4>
+                <h2 class="descripcion__nombre">${product.nombre}</h2>
+                <p class="descripcion__precio">$${product.precio}</p>
+            </div>
+            <button onclick="addToCart(${product.id}, this)" class="card__btn-shop">
+                <i class="ri-shopping-cart-fill"></i>
+                Añadir al carrito
+            </button>
+            <div class="card__cantidad" id="card__cantidad-${product.id}">
+                <button class="cantidad__btn-remove" onclick="decrementarProducto(${product.id})">
+                    <i class= "ri-indeterminate-circle-line"></i>
+                </button>
+                <div class="card__numero" id="cantidad-${product.id}" data-id="${product.id}">
+                    1
+                </div>
+                <button class="cantidad__btn-add" onclick="incrementarProducto(${product.id})">
+                    <i class="ri-add-circle-line"></i>
+                </button>
+            </div>
+
+        `;
+        productList.appendChild(article);
+    });
+
+}
+
+//Funcion para añadir productos al carrito
+function addToCart(productId, buttonElement) {
+    const cantidadElement = document.getElementById(`cantidad-${productId}`);
+    const quantity = parseInt(cantidadElement.textContent, 10);
+
+    const existingProduct = cart.find(item => item.id === productId);
+
+    if(existingProduct){
+        existingProduct.quantity += quantity;
+    }else{
+        fetch('productos.json')
+        .then(response => response.json())
+        .then(productos => {
+            const product = productos.find(p => p.id === productId);
+            if(product){
+                cart.push({...product, quantity: quantity});
+                displayCart();
+            }
+        })
+        .catch(error => console.error('Error al obtener el producto', error));
+    }
+    
+    buttonElement.style.display = 'none';
+
+    const cantidadContainer = buttonElement.nextElementSibling;
+    cantidadContainer.style.display = 'flex';
+
+    const cardImgElement = buttonElement.closest('.cards__card').querySelector('.card__img');
+    cardImgElement.classList.add('active');
+}
+
+//Funcion para incrementar la cantidad de un producto
+function incrementarProducto(productId) {
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    let cantidad = parseInt(cantidadElemento.textContent, 10);
+    cantidad++;
+    cantidadElemento.textContent = cantidad;
+
+    const producto = cart.find(item => item.id === productId);
+    if(producto){
+        producto.quantity = cantidad;
+    }   
+
+    displayCart();
+}
+
+/*Funcion para decrementar la cantidad de un producto*/
+function decrementarProducto(productId) {
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    let cantidad = parseInt(cantidadElemento.textContent, 10);
+
+    if(cantidad > 1){
+        cantidad--;
+        cantidadElemento.textContent = cantidad;
+    }else{
+        cantidad = 0;
+
+        cart = cart.filter(item => item.id !== productId);
+
+        const cantidadContainer = cantidadElemento.parentElement;
+        const buttonElement = cantidadContainer.previousElementSibling;
+        cantidadContainer.style.display = 'none';
+        buttonElement.style.display = 'inline-block';
+
+        const cardImgElement = buttonElement.closest('.cards__card').querySelector('.card__img');
+        cardImgElement.classList.remove('active');
+    }
+
+    const producto = cart.find(item => item.id === productId);
+    if(producto){
+        producto.quantity = cantidad;
+    }
+
+    displayCart();
+}
+
+//Funcion para eliminar un producto del carrito
+function removeFromCart(productId){
+    cart = cart.filter(item => item.id !== productId);
+
+    const cantidadContainer = document.getElementById(`cantidad-${productId}`).parentElement;
+    const buttonElement = cantidadContainer.previousElementSibling;
+    cantidadContainer.style.display = 'none';
+    buttonElement.style.display = 'inline-block';
+
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    cantidadElemento.textContent = 1;
+
+    const cardImgElement = buttonElement.closest('.cards__card').querySelector('.card__img');
+    cardImgElement.classList.remove('active');
+
+    displayCart();
+}
+
+//Funcion para mostrar los productos en el carrito
+function displayCart() {
+    const cartList = document.querySelector('.cart-list__items');
+    const cartHeader = document.querySelector('.cart-list h2');
+    cartList.innerHTML = '';
+
+    let total = 0;
+
+    if(cart.length === 0){// ESTA PARTE ES PARA QUE SE MUESTRE EL LOGO DESPUES DE ELIMINAR UN PRODUCTO DEL CARRITO
+        cartList.innerHTML = `
+            <div class="items__img">
+                <img src="imagenes/logo.png" alt="Logo carrito">
+            </div>
+            <p>Tu ropa aparecerán aquí</p>
+        `;
+    }else{
+        cart.forEach(item => {
+            total += item.precio * item.quantity;
+            cartList.innerHTML += `
+                <div class="items__item">
+                    <h4> ${item.nombre}</h4>
+                    <div class="item__detalles">
+                        <p>${item.quantity}x <span>$${item.precio.toFixed(2)}</span></p>
+                        <p>$${(item.precio.toFixed(2) * item.quantity).toFixed(2)}</p>
+                        <button onclick="removeFromCart(${item.id})">
+                            <i class="ri-close-circle-line"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+        });
+
+        const totalContainer = document.createElement('div');
+        totalContainer.classList.add('item__total');
+        totalContainer.innerHTML =  `
+            <div>
+                <span>Total a cancelar: </span>
+                <span class="total__monto">$${total.toFixed(2)}</span>
+            </div>
+            <button class="btn" id="btn-checkout">Realizar pedido</button>
+        `;
+        cartList.appendChild(totalContainer);
+    }
+
+    const checkoutButton = document.getElementById('btn-checkout');
+    if(checkoutButton){
+        checkoutButton.addEventListener('click', mostrarModalPedido);
+    }
+    const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+    cartHeader.textContent = `Tu carrito (${totalItems})`; //ME QUEDE EN EL MIN 52:10
+
+}
+
+
+//Funcion para mostrar el modal del pedido
+function mostrarModalPedido(){
+    const pedidoDetalle = document.querySelector(".pedido__detalle");
+    pedidoDetalle.innerHTML = '';
+
+    let total = 0;
+
+    cart.forEach(item => {
+        total += item.precio * item.quantity;
+
+        const detalleItem = document.createElement('div');
+        detalleItem.classList.add('detalle__item');
+        detalleItem.innerHTML = `
+            <div class="item__pedido">
+                <div class="pedido__img">
+                    <img src="${item.imagen}" alt="${item.nombre}">
+                </div>
+                <div class="pedido__lista">
+                    <p>${item.nombre}</p>
+                    <p>${item.quantity} x <span>$${(item.precio).toFixed(2)}</span></p>
+                </div>
+                <p></p>
+        `;
+        pedidoDetalle.appendChild(detalleItem);
+    });
+
+    const totalElement = document.createElement('div');
+    totalElement.classList.add('pedido__total');
+    totalElement.innerHTML = `
+        <h4>Total:</h4>
+        <h4>$${total.toFixed(2)}</h4>
+    `;
+    pedidoDetalle.appendChild(totalElement);
+
+    document.getElementById('overlay').classList.add('active');
+    document.getElementById('modal-pedido').classList.add('active');
+}
+
+//Funcion para cerrar el modal de pedido y resetear el estado del carrito
+function cerrarModalPedido(){
+    document.getElementById('overlay').classList.remove('active');
+    document.getElementById('modal-pedido').classList.remove('active');
+
+    const cardImages = document.querySelectorAll('.card__img.active');
+    cardImages.forEach(cardImg => {
+        cardImg.classList.remove('active');
+    });
+
+    const cantidadContainers = document.querySelectorAll('.card__cantidad');
+    cantidadContainers.forEach(cantidadContainer => {
+        cantidadContainer.querySelector('.cantidad__numero').textContent = '1';
+        cantidadContainer.style.display = none;
+
+        const addToCartButton = cantidadContainer.previousElementSibling;
+        if(addToCartButton.classList.contains('card__btn-shop')){
+            addToCartButton.style.display = 'block';
+        }
+    });
+
+    const cartHeader = document.querySelector('.cart-list h2');
+    cartHeader.textContent= 'Tu carrito (0)';
+
+    cart.length= 0;
+    displayCart();
+}
+
+document.getElementById('btn-close').addEventListener('click',cerrarModalPedido);
+
+loadProducts();
